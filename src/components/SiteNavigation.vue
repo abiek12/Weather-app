85af80d4c8c67ed4180043b3f26afcf86e5de260
Abhishek p p <abhishekkanichery@gmail.com>
2024-03-09 23:58:07 +0530

    Inital commit

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-8">
+      <div class="flex items-center gap-3 flex-1"></div>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
