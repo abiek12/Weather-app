f02f037e8350a5b1bfe6a5b246b1ab4f00e45a9b
Abhishek p p <abhishekkanichery@gmail.com>
2024-03-10 19:25:28 +0530

    implements local offset time zone

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -15,6 +15,16 @@ const getWeatheraData = async () => {
     const weatherData = await Axios.get(
       `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${weatherAPIKey}&units=imperial`
     )
+    // cal current date & time
+    const localOffset = new Date().getTimezoneOffset() * 60000
+    const utc = weatherData.data.current.dt * 1000 + localOffset
+    weatherData.data.currentTime = utc + 1000 * weatherData.data.timezone_offset
+
+    // cal hourly weather offset
+    weatherData.data.hourly.forEach((hour) => {
+      const utc = hour.dt * 1000 + localOffset
+      hour.currentTime = utc + 1000 * weatherData.data.timezone_offset
+    })
     return weatherData
   } catch (error) {
     console.log(error)
