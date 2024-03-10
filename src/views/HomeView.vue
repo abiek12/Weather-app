76dbf1b06f9ccd4d471057c12c2d8f3c9a5e6d49
Abhishek p p <abhishekkanichery@gmail.com>
2024-03-10 11:40:15 +0530

    creates get weather data function using axios and set open weather api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@
     <div class="pt-4 mb-8 relative">
       <input
         type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
         placeholder="Search for a city or state"
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
       />
@@ -10,4 +12,26 @@
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { Axios } from 'axios'
+import ref from 'vue'
+
+const mapboxAPIKey = '0cc9e70ed1c45d1f75ad22b3365aba0c'
+const searchQuery = ref('')
+const queryTimeOut = ref(null)
+const mapboxSearchResults = ref(null)
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeOut.value)
+  queryTimeOut.value = setTimeout(async () => {
+    if (searchQuery.value !== '') {
+      const result = await Axios.get(
+        `http://api.openweathermap.org/data/2.5/forecast?id=${searchQuery}&appid=${mapboxAPIKey}`
+      )
+      mapboxSearchResults.value = result.data.features
+      return
+    }
+    mapboxSearchResults.value = null
+  }, 300)
+}
+</script>
