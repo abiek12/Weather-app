6dadfa5ec35da76ef4e5c4e6c13f7e4354bcf176
Abhishek p p <abhishekkanichery@gmail.com>
2024-03-10 15:02:17 +0530

    modified search city function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,25 @@
         placeholder="Search for a city or state"
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
       />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
+        <p class="py-2" v-if="searchError">Sorry, something went wrong, please try again.</p>
+        <p class="py-2" v-if="!searchError && mapboxSearchResults.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in mapboxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)"
+          >
+            {{ searchResult.name }}
+          </li>
+        </template>
+      </ul>
     </div>
   </main>
 </template>
@@ -25,14 +44,25 @@ const getSearchResults = () => {
   clearTimeout(queryTimeOut.value)
   queryTimeOut.value = setTimeout(async () => {
     if (searchQuery.value !== '') {
-      const result = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${searchQuery.value}&limit=5&appid=${mapboxAPIKey}`
-      )
-      mapboxSearchResults.value = result.data.features
-      console.log(mapboxSearchResults)
-      return
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/geo/1.0/direct?q=${searchQuery.value}&limit=5&appid=${mapboxAPIKey}`
+        )
+        if (response.data && response.data.length > 0) {
+          mapboxSearchResults.value = response.data.map((city) => ({
+            id: city.name,
+            name: city.name
+          }))
+        } else {
+          mapboxSearchResults.value = null
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error)
+        mapboxSearchResults.value = null
+      }
+    } else {
+      mapboxSearchResults.value = null
     }
-    mapboxSearchResults.value = null
   }, 300)
 }
 </script>
