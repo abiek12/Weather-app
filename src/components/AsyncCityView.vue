f1c3eaf80b2cff148c41263e97f3f23aea43e55b
Abhishek p p <abhishekkanichery@gmail.com>
2024-03-11 23:39:21 +0530

    rendered out the 5days wether forecast

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -68,6 +68,29 @@
     </div>
     <hr class="border-white border-opacity-10 border w-full" />
     <!-- Weekly weather -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">5 Day Forecast</h2>
+        <div v-for="day in weatherData.data.list" :key="day.dt" class="flex items-center">
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString('en-us', {
+                weekday: 'long'
+              })
+            }}
+          </p>
+          <img
+            class="w-[50px] h-[50px] object-cover"
+            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            alt=""
+          />
+          <div class="flex gap-2 flex-1 justify-end">
+            <p>H: {{ Math.round(day.main.temp_max) }}</p>
+            <p>L: {{ Math.round(day.main.temp_min) }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
