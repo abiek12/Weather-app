8543c06cac7d750ea78b5c47ad23841683f940dd
Abhishek p p <abhishekkanichery@gmail.com>
2024-03-16 17:43:05 +0530

    added titles for each page

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,27 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Home'
+      }
     },
     {
       path: '/weather/:state/:city',
       name: 'cityview',
-      component: CityView
+      component: CityView,
+      meta: {
+        title: 'Weather'
+      }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${
+    to.params.state ? `${to.params.city}, ${to.params.state}` : to.meta.title
+  } | The Weather`
+  next()
 })
 
 export default router
